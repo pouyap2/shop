0420f92a58d38c8bb8366c4df8542af1242e6429
pouyap2 <pouya.prd@gmail.com>
2022-07-11 21:16:27 +0430

    web template added to the project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home');
+});
+
+Route::get('/adminpanel',function (){
+return view('admin.home');
 });
